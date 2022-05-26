7375692b735ade45266f3a51c834f83406a0a81e
zhazhalaila <1176694275@qq.com>
2022-05-26 21:03:37 +0800

    add incoming register func

--- src/libnet/transport.go
+++ src/libnet/transport.go
@@ -3,45 +3,51 @@ package libnet
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"log"
 	"math"
 	"net"
-
-	"github.com/sasha-s/go-deadlock"
 )
 
 const (
 	bufSize = math.MaxUint16
 )
 
+var (
+	// ErrTransportStop is returned when stop channel is closed
+	ErrTransportStop = errors.New("Transport shutdown")
+)
+
 type NetworkTransport struct {
 	logger *log.Logger
 
-	mu deadlock.RWMutex
-
 	port     string
 	listener net.Listener
-	peers    map[int]bufConn
 
-	logInCh   chan client
-	logOutCh  chan string
+	logInCh  chan remoteConn
+	logOutCh chan string
+
+	peerRegisterCh chan peer
+
+	// Send data to consensus module
 	consumeCh chan interface{}
+	// Read data from consensus module and broadcast data to all peers
+	peerBroadcastCh chan interface{}
+	// Read consens result from consensus module
 	resultCh  chan interface{}
 	stopCh    chan struct{}
 	releaseCh chan struct{}
 }
 
-type client struct {
+type remoteConn struct {
 	remoteAddr string
 	enc        *json.Encoder
-	responseCh chan interface{}
 }
 
-type bufConn struct {
+type peer struct {
+	id   int
 	conn net.Conn
-	r    *bufio.Reader
 	w    *bufio.Writer
-	dec  *json.Decoder
 	enc  *json.Encoder
 }
 
@@ -49,18 +55,21 @@ func NewNetworkTransport(logger *log.Logger, port string) *NetworkTransport {
 	nt := &NetworkTransport{}
 	nt.logger = logger
 	nt.port = port
-	nt.peers = make(map[int]bufConn)
-	nt.logInCh = make(chan client, 100)
+	nt.logInCh = make(chan remoteConn, 100)
 	nt.logOutCh = make(chan string, 100)
+	nt.peerRegisterCh = make(chan peer, 100)
 	nt.consumeCh = make(chan interface{}, 10000)
+	nt.peerBroadcastCh = make(chan interface{}, 10000)
 	nt.stopCh = make(chan struct{})
 	nt.releaseCh = make(chan struct{})
 
-	go nt.clientManger()
+	go nt.inComingManger()
+	go nt.peerManger()
 
 	return nt
 }
 
+// Start network
 func (nt *NetworkTransport) Start() {
 	var err error
 	nt.listener, err = net.Listen("tcp", nt.port)
@@ -85,43 +94,95 @@ func (nt *NetworkTransport) Start() {
 	}
 }
 
-// clientManger mange client state and send response to client
-func (nt *NetworkTransport) clientManger() {
-	clients := make(map[string]client)
+// inComing manger mange client state and send response to client
+func (nt *NetworkTransport) inComingManger() {
+	inConns := make(map[string]remoteConn)
 	for {
 		select {
 		case <-nt.stopCh:
 			return
 
 		case client := <-nt.logInCh:
-			if _, ok := clients[client.remoteAddr]; ok {
-				nt.logger.Printf("[Client:%s] has been registered.\n", client.remoteAddr)
-				break
+			if _, ok := inConns[client.remoteAddr]; !ok {
+				inConns[client.remoteAddr] = client
 			}
-			clients[client.remoteAddr] = client
 
 		case remoteAddr := <-nt.logOutCh:
-			delete(clients, remoteAddr)
+			delete(inConns, remoteAddr)
 
 		case msg := <-nt.resultCh:
-			clients[msg.(string)].enc.Encode(msg)
+			inConns[msg.(string)].enc.Encode(msg)
+
+		default:
+			continue
 		}
+	}
+}
+
+// peer manager manage peer state and send data to peer
+func (nt *NetworkTransport) peerManger() {
+	peers := make(map[int]peer)
+	for {
+		select {
+		case <-nt.stopCh:
+			return
+
+		case peer := <-nt.peerRegisterCh:
+			if _, ok := peers[peer.id]; !ok {
+				peers[peer.id] = peer
+			}
+
+		case msg := <-nt.peerBroadcastCh:
+			for id, peer := range peers {
+				if err := peer.enc.Encode(msg); err != nil {
+					nt.logger.Printf("Marshal data (send to peer:%d) error: %s.\n", id, err.Error())
+				}
+				if err := peer.w.Flush(); err != nil {
+					nt.logger.Printf("Send data to peer:%d error: %s.\n", id, err.Error())
+				}
+			}
+		}
+
 	}
 }
 
 func (nt *NetworkTransport) handleConn(conn net.Conn) {
 	defer func() {
-		// if peer close connection will cause no-op
 		nt.logOutCh <- conn.RemoteAddr().String()
 		conn.Close()
 	}()
 
-	// r := bufio.NewReaderSize(conn, bufSize)
-	// w := bufio.NewWriterSize(conn, bufSize)
-	// dec := json.NewDecoder(r)
-	// enc := json.NewEncoder(w)
+	r := bufio.NewReaderSize(conn, bufSize)
+	w := bufio.NewWriterSize(conn, bufSize)
+	dec := json.NewDecoder(r)
+	enc := json.NewEncoder(w)
 
-	// for {
+	select {
+	case <-nt.stopCh:
+		return
+	case nt.logInCh <- remoteConn{remoteAddr: conn.RemoteAddr().String(), enc: enc}:
+	}
 
-	// }
+	for {
+		if err := nt.handleCommand(r, dec); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func (nt *NetworkTransport) handleCommand(r *bufio.Reader, dec *json.Decoder) error {
+	var req interface{}
+
+	if err := dec.Decode(req); err != nil {
+		return err
+	}
+
+	select {
+	case <-nt.stopCh:
+		return ErrTransportStop
+	case nt.consumeCh <- req:
+	}
+
+	return nil
 }
